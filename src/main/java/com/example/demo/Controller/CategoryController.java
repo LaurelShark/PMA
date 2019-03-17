2e6b1f8004c116e0101ab31efcf0bd2c8521cdab
Yaroslav Kladko <yarikhaker@gmail.com>
2019-03-17 23:28:08 +0200

    cors allowed

--- src/main/java/com/example/demo/Controller/CategoryController.java
+++ src/main/java/com/example/demo/Controller/CategoryController.java
@@ -4,10 +4,7 @@ import com.example.demo.Entity.Category;
 import com.example.demo.Service.CategoryService;
 import com.example.demo.Service.impl.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +14,7 @@ public class CategoryController {
 
     @Autowired
     private CategoryServiceImpl categoryServiceImpl;
-
+    
     @GetMapping
     public Iterable<Category> getAll(){
         Iterable<Category> categories = null;
