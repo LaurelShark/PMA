531740f2d90176fa0f2cfdc0956b944915491b90
Yaroslav Kladko <yarikhaker@gmail.com>
2019-04-22 12:27:45 +0300

    upd database

--- trading.sql
+++ trading.sql
@@ -16,35 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `availability`
---
-
-DROP TABLE IF EXISTS `availability`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `availability` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `amount` float NOT NULL,
-  `price` float NOT NULL,
-  `good_id` int(11) NOT NULL,
-  `min_amount` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `good_id` (`good_id`),
-  CONSTRAINT `availability_ibfk_1` FOREIGN KEY (`good_id`) REFERENCES `goods` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `availability`
---
-
-LOCK TABLES `availability` WRITE;
-/*!40000 ALTER TABLE `availability` DISABLE KEYS */;
-INSERT INTO `availability` VALUES (1,10,15.5,1,2),(2,1,200000,3,1),(3,2,100000,2,2);
-/*!40000 ALTER TABLE `availability` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `categories`
 --
 
@@ -55,7 +26,7 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +35,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'grocery'),(2,'sedan'),(3,'SUV'),(4,'Jomn');
+INSERT INTO `categories` VALUES (1,'grocery'),(2,'sedan'),(3,'SUV'),(4,'Jomn'),(5,'smartphones'),(6,'earphones'),(7,'cases'),(8,'power-banks'),(9,'categoria');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,7 +51,7 @@ CREATE TABLE `departments` (
   `manager_id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,7 +60,7 @@ CREATE TABLE `departments` (
 
 LOCK TABLES `departments` WRITE;
 /*!40000 ALTER TABLE `departments` DISABLE KEYS */;
-INSERT INTO `departments` VALUES (1,1,'food'),(2,2,'cars');
+INSERT INTO `departments` VALUES (1,1,'food'),(2,2,'cars'),(3,1,'Phones'),(4,2,'Accessories'),(5,1,'Management'),(6,2,'Stock');
 /*!40000 ALTER TABLE `departments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +85,7 @@ CREATE TABLE `goods` (
   KEY `department_id` (`department_id`),
   CONSTRAINT `goods_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
   CONSTRAINT `goods_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +94,7 @@ CREATE TABLE `goods` (
 
 LOCK TABLES `goods` WRITE;
 /*!40000 ALTER TABLE `goods` DISABLE KEYS */;
-INSERT INTO `goods` VALUES (1,'stew',1,1,0,0,0,NULL),(2,'Mercedes Benz',2,2,0,0,0,NULL),(3,'Mercedes Gelandewagen',3,2,0,0,0,NULL);
+INSERT INTO `goods` VALUES (1,'stew',1,1,0,0,0,NULL),(2,'Mercedes Benz',2,2,0,0,0,NULL),(3,'Mercedes Gelandewagen',3,2,0,0,0,NULL),(5,'Good1',1,1,2,1,30,'SSL-12LTS-95-HS-112'),(6,'Phone Samsung Galaxy S10 Black',1,1,19,4,48000,'35-4190'),(7,'iPhone S8 Rose Gold',1,1,6,4,35000,'11-419002-389644-6'),(8,'Sony Noise Cancelin Headphones',2,2,28,5,500,NULL),(9,'Sony Stereo Wired Earbuds',2,2,30,5,299,NULL),(10,'XIAOMI Mi Power Bank',4,2,13,7,899,NULL),(11,'Silicon Case for iPhone S5',3,2,63,10,679,NULL),(12,'knut',1,1,100,5,90,NULL),(13,'knut32',1,1,100,5,90,NULL);
 /*!40000 ALTER TABLE `goods` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,7 +119,7 @@ CREATE TABLE `orderlines` (
   CONSTRAINT `orderlines_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   CONSTRAINT `orderlines_ibfk_2` FOREIGN KEY (`good_id`) REFERENCES `goods` (`id`),
   CONSTRAINT `orderlines_ibfk_3` FOREIGN KEY (`provider_id`) REFERENCES `providers` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,6 +128,7 @@ CREATE TABLE `orderlines` (
 
 LOCK TABLES `orderlines` WRITE;
 /*!40000 ALTER TABLE `orderlines` DISABLE KEYS */;
+INSERT INTO `orderlines` VALUES (4,20,48000,1,1,2),(5,6,35000,2,2,1),(6,63,679,2,6,2);
 /*!40000 ALTER TABLE `orderlines` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,7 +148,7 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,6 +157,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'done','2019-02-21',960000,2),(2,'done','2019-02-27',252777,2);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,7 +177,7 @@ CREATE TABLE `providers` (
   `card_number` varchar(16) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -213,6 +186,7 @@ CREATE TABLE `providers` (
 
 LOCK TABLES `providers` WRITE;
 /*!40000 ALTER TABLE `providers` DISABLE KEYS */;
+INSERT INTO `providers` VALUES (1,'Providers Group','Galyna Vasylkivska','0445673812','Baseina str, 9A, off.3','4876234509189754',NULL),(2,'Providers and Co.','Hlib Kovaliov','0447200918','Vokzalna str., 14, off.1','4782193082761090',NULL),(3,'Providers Group','Galyna Vasylkivska','0445673812','Baseina str, 9A, off.3','4876234509189754',NULL),(4,'Providers and Co.','Hlib Kovaliov','0447200918','Vokzalna str., 14, off.1','4782193082761090',NULL);
 /*!40000 ALTER TABLE `providers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -228,7 +202,7 @@ CREATE TABLE `receiptlines` (
   `name` varchar(30) NOT NULL,
   `amount` float NOT NULL,
   `price` float NOT NULL,
-  `unique_id` int(11) DEFAULT NULL,
+  `unique_id` varchar(100) DEFAULT NULL,
   `good_id` int(11) NOT NULL,
   `receipt_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -236,7 +210,7 @@ CREATE TABLE `receiptlines` (
   KEY `receipt_id` (`receipt_id`),
   CONSTRAINT `receiptlines_ibfk_1` FOREIGN KEY (`good_id`) REFERENCES `goods` (`id`),
   CONSTRAINT `receiptlines_ibfk_2` FOREIGN KEY (`receipt_id`) REFERENCES `receipts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -245,6 +219,7 @@ CREATE TABLE `receiptlines` (
 
 LOCK TABLES `receiptlines` WRITE;
 /*!40000 ALTER TABLE `receiptlines` DISABLE KEYS */;
+INSERT INTO `receiptlines` VALUES (13,'Phone Samsung Galaxy S10 Black',1,48000,'34-419002-389644-3',1,1),(14,'Sony Stereo Wired Earbuds',1,299,NULL,2,2),(15,'XIAOMI Mi Power Bank',1,899,NULL,3,2),(16,'Silicon Case for iPhone S5',1,679,NULL,2,2);
 /*!40000 ALTER TABLE `receiptlines` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -260,10 +235,11 @@ CREATE TABLE `receipts` (
   `date` date NOT NULL,
   `total_sum` float NOT NULL,
   `user_id` int(11) NOT NULL,
+  `datetime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `receipts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -272,6 +248,7 @@ CREATE TABLE `receipts` (
 
 LOCK TABLES `receipts` WRITE;
 /*!40000 ALTER TABLE `receipts` DISABLE KEYS */;
+INSERT INTO `receipts` VALUES (1,'2019-03-01',48000,3,NULL),(2,'2019-03-01',1877,3,NULL),(3,'2019-03-01',48000,3,NULL),(4,'2019-03-01',1877,3,NULL),(5,'2019-03-01',48000,9,NULL),(6,'2019-03-01',1877,10,NULL);
 /*!40000 ALTER TABLE `receipts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -310,14 +287,15 @@ CREATE TABLE `supplies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `provider_id` int(11) NOT NULL,
-  `total_sum` int(11) DEFAULT NULL,
+  `total_sum` float DEFAULT NULL,
   `order_id` int(11) NOT NULL,
+  `datetime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `provider_id` (`provider_id`),
   KEY `order_id` (`order_id`),
   CONSTRAINT `supplies_ibfk_1` FOREIGN KEY (`provider_id`) REFERENCES `providers` (`id`),
   CONSTRAINT `supplies_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -326,6 +304,7 @@ CREATE TABLE `supplies` (
 
 LOCK TABLES `supplies` WRITE;
 /*!40000 ALTER TABLE `supplies` DISABLE KEYS */;
+INSERT INTO `supplies` VALUES (2,'2019-02-24',1,210000,2,NULL),(3,'2019-02-24',2,960000,1,NULL),(4,'2019-02-25',2,294777,2,NULL);
 /*!40000 ALTER TABLE `supplies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -347,7 +326,7 @@ CREATE TABLE `supplylines` (
   KEY `supply_id` (`supply_id`),
   CONSTRAINT `supplylines_ibfk_1` FOREIGN KEY (`good_id`) REFERENCES `goods` (`id`),
   CONSTRAINT `supplylines_ibfk_2` FOREIGN KEY (`supply_id`) REFERENCES `supplies` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -356,6 +335,7 @@ CREATE TABLE `supplylines` (
 
 LOCK TABLES `supplylines` WRITE;
 /*!40000 ALTER TABLE `supplylines` DISABLE KEYS */;
+INSERT INTO `supplylines` VALUES (2,20,48000,1,2),(3,6,35000,2,4),(4,63,679,3,3);
 /*!40000 ALTER TABLE `supplylines` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -375,7 +355,7 @@ CREATE TABLE `userroles` (
   KEY `role_id` (`role_id`),
   CONSTRAINT `userroles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `userroles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -427,4 +407,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-17 16:10:27
+-- Dump completed on 2019-04-22 12:26:30
